7ea58be31afc4e57105e78039a008709949975a6
Mason <chtuff@gmail.com>
2024-01-20 21:56:47 -0500

    done for the nigth

--- js/main.js
+++ js/main.js
@@ -2,11 +2,8 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const poke1 = document.querySelector('#poke1').value
-  const poke2 = document.querySelector('#poke2').value
+  const poke1 = document.querySelector('#poke1').value.toLowerCase()
   const url = `https://pokeapi.co/api/v2/pokemon/${poke1}`
- 
-  const url2 = `https://pokeapi.co/api/v2/pokemon/${poke2}` 
   let pokeStore = []
   let pokeImg = []
 
@@ -14,42 +11,39 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        pokeStore.push(data.types[0].type.name)
-         pokeImg.push(data.sprites.other['official-artwork'].front_default)
-        
-        fetch(url2)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-          console.log(data)
-          pokeStore.push(data.types[0].type.name)
-          pokeImg.push(data.sprites.other['official-artwork'].front_default)
-      
-           document.querySelector('#pokeImg1').src = pokeImg[0]
-           document.querySelector('#pokeStore1').innerText = pokeStore[0]
-            document.querySelector('#pokeImg2').src = pokeImg[1]
-            document.querySelector('h2').innerText = " 2x > "
-
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
+        const notSureWhatIamDoingYet = new Poke (data.name, data.height, data.weight, data.types, data.sprites.other['official-artwork'].front_default)
+        notSureWhatIamDoingYet.getTypes()
 
 
+              pokeStore.push(data.types[0].type.name)
+              pokeImg.push(data.sprites.other['official-artwork'].front_default)
+              document.querySelector('#pokeImg1').src = pokeImg[0]
+              document.querySelector('#pokeStore1').innerText = pokeStore[0]
+              document.querySelector('h2').innerText = data.name
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+}
+        
 class Poke {
-  constructor(name, height, weight, types, image){
-    this.name,
-    this.height
-    this.types
-    this.image
-    this.weight
-    this.reason = []
-  }
+    constructor(name, height, weight, types, image){
+      this.name = name
+      this.height = height
+      this.types = types
+      this.image = image
+      this.weight = weight
+      this.undecidedPurpose = true
+      this.reason = []
+      this.typeList = []
 }
-
-
+      getTypes(){
+        for(const property of this.types) {
+            this.typeList.push(property.type.name)
+            }
+            console.log(this.typeList)
+          }
+      }
       
-}
+      
+      
