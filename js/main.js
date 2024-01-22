03a35dbc066ff3617b957e365b43f52d465bbf7d
Mason <chtuff@gmail.com>
2024-01-21 21:26:42 -0500

    done for the night

--- js/main.js
+++ js/main.js
@@ -1,25 +1,34 @@
-//Example fetch using pokemonapi.co
+
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const poke1 = document.querySelector('#poke1').value.toLowerCase()
-  const url = `https://pokeapi.co/api/v2/pokemon/${poke1}`
-  let pokeStore = []
-  let pokeImg = []
+  //const poke1 = document.querySelector('#poke1').value.toLowerCase()
+ // const url = `https://pokeapi.co/api/v2/pokemon/${poke1}`
+ // let pokeImg = []
+  const choice = document.querySelector('input').value.toLowerCase()
+  const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        //code I need to change
         const notSureWhatIamDoingYet = new Poke (data.name, data.height, data.weight, data.types, data.sprites.other['official-artwork'].front_default)
         notSureWhatIamDoingYet.getTypes()
+        notSureWhatIamDoingYet.isItUndecided()
+        let decision = ''
+        if(notSureWhatIamDoingYet.undecidedPurpose){
+          decision = 'blah blah blah'
+        }else {
+          decision = `This Pokemon has these types:${notSureWhatIamDoingYet.typeList} ${notSureWhatIamDoingYet.reason.join(' and ')}.`
+        }
+        document.querySelector('h2').innerText = decision
+        document.querySelector('img').src = notSureWhatIamDoingYet.image
 
-
-              pokeStore.push(data.types[0].type.name)
-              pokeImg.push(data.sprites.other['official-artwork'].front_default)
-              document.querySelector('#pokeImg1').src = pokeImg[0]
-              document.querySelector('#pokeStore1').innerText = pokeStore[0]
-              document.querySelector('h2').innerText = data.name
+              // pokeImg.push(data.sprites.other['official-artwork'].front_default)
+              // document.querySelector('#pokeImg1').src = pokeImg[0]
+              // document.querySelector('h3').innerText = data.name
+              //end of code I need to change 
       })
       .catch(err => {
           console.log(`error ${err}`)
@@ -36,14 +45,42 @@ class Poke {
       this.undecidedPurpose = true
       this.reason = []
       this.typeList = []
-}
-      getTypes(){
-        for(const property of this.types) {
-            this.typeList.push(property.type.name)
-            }
-            console.log(this.typeList)
-          }
+    }
+
+  getTypes(){
+      for(const property of this.types) {
+          this.typeList.push(property.type.name)
       }
+    console.log(this.typeList)
+  }
+
+  weightToPounds(bacon){
+      return Math.round((bacon/4.536)*100)/100
+  }
+
+  heightToFeet(height){
+    return Math.round((height/3.048)*100)/100
+  }
+
+  isItUndecided() {
+    let badTypes = ['fire', 'electric', 'fighting', 'poison', 'ghost']
+    if (this.weightToPounds(this.weight) > 400)
+    {
+      this.reason.push(`it is too heavy at 
+        ${this.weightToPounds(this.weight)} pounds`)
+        this.undecidedPurpose = false
+    }
+    if (this.heightToFeet(this.height) > 7) {
+      this.reason.push(`it is too tall at 
+      ${this.heightToFeet(this.height)} feet`)
+      this.undecidedPurpose = false
+    }
+    if (badTypes.some(r=> this.typeList.indexOf(r) >= 0)){
+      this.reason.push(`${this.name} type is too dangerous`)
+      this.undecidedPurpose = false
+    }
+  }
+}
       
       
       
