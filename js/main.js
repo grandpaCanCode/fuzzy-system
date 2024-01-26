a4871702a04b05eebd59e165b50a9fe8308c51ae
Mason <chtuff@gmail.com>
2024-01-26 18:57:45 -0500

    figuring out what data to use

--- js/main.js
+++ js/main.js
@@ -7,30 +7,25 @@ function getFetch(){
   const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
+      .then(res => res.json()) 
       .then(data => {
         console.log(data)
         //code I need to change
         const notSureWhatIamDoingYet = new PokeInfo (data.name, data.abilities, data.height, data.weight, data.types, data.sprites.other['official-artwork'].front_default, data.location_area_encounters)
         notSureWhatIamDoingYet.getTypes()
-        notSureWhatIamDoingYet.isItUndecided()
+       
         notSureWhatIamDoingYet.getAbilities()
         notSureWhatIamDoingYet.encounterInfo()
-        let decision = ''
-        if(notSureWhatIamDoingYet.undecidedPurpose){
-          decision = `${notSureWhatIamDoingYet.name} 
-                      has these types: ${notSureWhatIamDoingYet.typeList}
-                      with these abilities: ${notSureWhatIamDoingYet.abilitiesList}`
-        }else {
-          decision = `${notSureWhatIamDoingYet.name} has these types:
+        
+        let decision = `${notSureWhatIamDoingYet.name} has these type(s):
                       ${notSureWhatIamDoingYet.typeList} 
-                      with these abilities: ${notSureWhatIamDoingYet.abilitiesList}
-                      ${notSureWhatIamDoingYet.reason.join(' and ')}
+                      with these abilitie(s): ${notSureWhatIamDoingYet.abilitiesList}
                       At these location(s):`
                       notSureWhatIamDoingYet.encounterInfo()
-        }
+       
         document.querySelector('h2').innerText = decision
         document.querySelector('img').src = notSureWhatIamDoingYet.image
+        document.getElementById('locations').innerText = ''
 
          
       })
@@ -75,23 +70,7 @@ class Poke {
     return Math.round((height/3.048)*100)/100
   }
 
-  isItUndecided() {
-    let badTypes = ['fire', 'electric', 'fighting', 'poison', 'ghost']
-    if (badTypes.some(r=> this.typeList.indexOf(r) >= 0)){
-      this.reason.push(`. Those types are too dangerous.`)
-      this.undecidedPurpose = false
-    }
-    if (this.weightToPounds(this.weight) > 400){
-      this.reason.push(`${this.name} is too heavy at 
-        ${this.weightToPounds(this.weight)} pounds`)
-        this.undecidedPurpose = false
-    }
-    if (this.heightToFeet(this.height) > 7) {
-      this.reason.push(`${this.name} is too tall at 
-      ${this.heightToFeet(this.height)} feet`)
-      this.undecidedPurpose = false
-    }
-  }
+  
 }
 class PokeInfo extends Poke {
     constructor(name, abilities, height, weight, types, image, location){
